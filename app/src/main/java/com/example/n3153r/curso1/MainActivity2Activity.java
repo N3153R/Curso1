c89c0e1b5ccea06f90d20ba232e65b4d95c7d25a
--global <neiss.rodriguez@gmail.com>
2016-09-12 15:17:29 -0500

    12

--- app/src/main/java/com/example/n3153r/curso1/MainActivity2Activity.java
+++ app/src/main/java/com/example/n3153r/curso1/MainActivity2Activity.java
@@ -1,18 +1,62 @@
 package com.example.n3153r.curso1;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity2Activity extends AppCompatActivity {
+public class MainActivity2Activity extends AppCompatActivity implements View.OnClickListener {
 
     TextView textView;
+    Button boton1,boton2;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2);
+
+//        -----------
+//        BOTON DIALOG
+
+        boton1 = (Button) findViewById(R.id.btnClick);
+
+
+
+        boton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                alertDialogBuilder.setMessage("Mensaje")
+                        .setCancelable(false)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Lo que quieras hace cuando se da click a "SI"
+
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Lo que quieras hace cuando se da click a "SI"
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialogBuilder.show();
+            }
+        });
+
+
+//        --------------
 
         textView =(TextView) findViewById(R.id.texto);
 
@@ -25,6 +69,31 @@ public class MainActivity2Activity extends AppCompatActivity {
             textView.setText(cadena);
 
         }
+//-------------------------------
+//        BOTON DIALOGO IMAGEN
+
+        boton2 = (Button) findViewById(R.id.btnClick2);
+        boton2.setOnClickListener(this);
 
     }
+
+
+    protected Dialog onCreateDialog(int id){
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder = builder.setIcon(R.drawable.mayte);
+        builder = builder.setTitle("Mi primer dialogo");
+
+        dialog = builder.create();
+
+        return dialog;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        onCreateDialog(0).show();
+    }
+
+//    -----------------
 }
--- app/src/main/java/com/example/n3153r/curso1/MainActivity3Activity.java
+++ app/src/main/java/com/example/n3153r/curso1/MainActivity3Activity.java
@@ -1,13 +1,23 @@
 package com.example.n3153r.curso1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity3Activity extends AppCompatActivity {
+public class MainActivity3Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity3);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity3Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },4000);
     }
 }
